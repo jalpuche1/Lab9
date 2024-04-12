45d82e27d6bc9e1701e3cc044c98d0b4a72b6870
jalpuche1 <jalpuche114@gmail.com>
2024-04-12 18:54:48 -0400

    Update main.py
    lab 9

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-def password_encoder(password):
-    encoded_password = ""
-    for digit in password:
-        encoded_digit = str((int(digit) + 3) % 10)  # Shifting each digit up by 3 numbers
-        encoded_password += encoded_digit
-    return encoded_password
+def encode(password):
+    encoded = ""
+    for i in range(len(password)):
+        encoded += str((int(password[i]) + 3) % 10)
+    return encoded
+
+def decode(encoded_password):
+    decoded = ""
+    for i in range(len(encoded_password)):
+        decoded += str((int(encoded_password[i]) - 3) % 10)
+    return decoded
 
 while True:
-    print("Menu")
-    print("-------------")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Quit")
+    print(
+        """
+Menu  
+------------- 
+1. Encode  
+2. Decode  
+3. Quit 
+    """
+    )
 
-    choice = input("Please enter an option: ")
+    option = input("Please enter an option: ")
 
-    if choice == "1":
+    if option == "1":
         password = input("Please enter your password to encode: ")
-        encoded_password = password_encoder(password)
-        print("Your password has been encoded and stored!")
-    # elif choice == "2":
-    #     if 'encoded_password' not in locals():
-    #         print("Please encode a password first.")
-    #         continue
-    #     print(
-    #         f"The encoded password is {encoded_password}, and the original password is {password_decoder(encoded_password)}.")
-    elif choice == "3":
-        print("Exiting the program.")
-        break
-    else:
-        print("Invalid option. Please choose again.")
+        encoded_pass = encode(password)
+        print(encoded_pass)
+
+    elif option == "2":
+        encoded_pass = input("Please enter your encoded password to decode: ")
+        decoded_pass = decode(encoded_pass)
+        print(f'The encoded password is {encoded_pass}, and the original password is {decoded_pass}')
+
+    elif option == "3":
+        quit()
