118fc8542a49ee5b286e5f79b41e32768f230f2b
jalpuche1 <jalpuche114@gmail.com>
2024-04-12 18:41:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def password_decoder(encoded_password):
-    decoded_password = ""
-    for digit in encoded_password:
-        decoded_digit = str((int(digit) - 3) % 10)  # Shifting each digit back by 3 numbers
-        decoded_password += decoded_digit
-    return decoded_password
+def password_encoder(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)  # Shifting each digit up by 3 numbers
+        encoded_password += encoded_digit
+    return encoded_password
 
 while True:
     print("Menu")
@@ -14,16 +14,16 @@ while True:
 
     choice = input("Please enter an option: ")
 
-    # if choice == "1":
-    #     password = input("Please enter your password to encode: ")
-    #     encoded_password = password_encoder(password)
-    #     print("Your password has been encoded and stored!")
-    elif choice == "2":
-        if 'encoded_password' not in locals():
-            print("Please encode a password first.")
-            continue
-        print(
-            f"The encoded password is {encoded_password}, and the original password is {password_decoder(encoded_password)}.")
+    if choice == "1":
+        password = input("Please enter your password to encode: ")
+        encoded_password = password_encoder(password)
+        print("Your password has been encoded and stored!")
+    # elif choice == "2":
+    #     if 'encoded_password' not in locals():
+    #         print("Please encode a password first.")
+    #         continue
+    #     print(
+    #         f"The encoded password is {encoded_password}, and the original password is {password_decoder(encoded_password)}.")
     elif choice == "3":
         print("Exiting the program.")
         break
